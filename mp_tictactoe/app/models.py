3dd0b59a29d3c5a7575ad8d6f7e8a7cc8e19f408
Oka Kurniawan <kurniawano@ieee.org>
2020-10-15 13:33:20 +0800

    create models.py template.

---
+++ mp_tictactoe/app/models.py
@@ -0,0 +1,38 @@
+from app import db
+from app import login
+from datetime import datetime 
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+@login.user_loader
+def load_user(id):
+	return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    #####################################################
+    # Your code here
+    # Add winning column as integer, with default value 0
+    # Add draw column as integer, with default value 0
+    # Add score column as integer, with default value 0
+    ######################################################
+
+    winning = None
+    draw = None
+    score = None
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'<User {self.username:}>'
+
+    
