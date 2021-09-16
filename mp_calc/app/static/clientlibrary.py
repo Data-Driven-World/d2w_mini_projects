3c608456d0ba70d06752d90188e7c7ae2d356633
aleextw <lee.tze.wei.alex.joel@gmail.com>
2021-09-16 19:51:53 +0800

    Fix HTTP Error 500 when pressing 'enter' for challenge
    After entering answer in text field on challenge page, pressing 'enter' circumvented javascript triggered by the submit button, resulting in HTTP error 500.
    
    Fix adds:
    - Event listener in clientlibrary.py to cancel default event (form submission) and instead trigger onClick event.
    - Element IDs in challenges.html for easier identification in JS code.

--- mp_calc/app/static/clientlibrary.py
+++ mp_calc/app/static/clientlibrary.py
@@ -25,8 +25,20 @@ class Records:
 	def __init__(self):
 		self.items = {}
 
+	def handle_return_keypress(self, question_id):
+		def handler(event):
+			if event.keyCode == 13:
+				event.preventDefault()
+				# Since the question_id is stored as the only key
+				document.getElementById(f"{question_id}-btn").click()
+		
+		return handler
+
 	def start_timer(self, question_id):
 		self.items[question_id] = AnswerTime(question_id)
+		answer_field = document.getElementById(question_id)
+		handler_func = self.handle_return_keypress(question_id)
+		answer_field.addEventListener("keyup", handler_func)
 
 	def stop_timer(self, form_id, question_id):
 		self.items[question_id].stop()
