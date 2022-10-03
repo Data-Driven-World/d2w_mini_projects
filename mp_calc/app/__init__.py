0bebd7058c2d5acb4a20ccc3a32bff3dca61f7de
Oka Kurniawan <oka@okakurniawan.net>
2022-10-03 18:06:51 +0800

    change to bootstrap5.

--- mp_calc/app/__init__.py
+++ mp_calc/app/__init__.py
@@ -3,7 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask_bootstrap import Bootstrap 
+from flask_bootstrap import Bootstrap5
 from app.middleware import PrefixMiddleware
 
 application = Flask(__name__)
@@ -12,7 +12,7 @@ db = SQLAlchemy(application)
 migrate = Migrate(application, db)
 login = LoginManager(application)
 login.login_view = 'login'
-bootstrap = Bootstrap(application)
+bootstrap = Bootstrap5(application)
 
 # set voc=False if you run on local computer
 application.wsgi_app = PrefixMiddleware(application.wsgi_app, voc=True)
