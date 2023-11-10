7ba2b9ea5c757240180fdcc483ae13d6991b1136
Oka Kurniawan <oka@okakurniawan.net>
2023-11-10 11:21:08 +0800

    add pipfile to template from mp_calc.

--- mp_template/app/middleware.py
+++ mp_template/app/middleware.py
@@ -1,23 +1,32 @@
 import os
 import socket
 
-class PrefixMiddleware(object):
 
+class PrefixMiddleware(object):
     def __init__(self, app, voc=True):
         self.app = app
         if voc:
-            myip = self.get_myip()
-            mytoken = os.getenv("VOC_PROXY_TOKEN")
-            self.prefix = f'/hostip/{myip}:5000/vocproxy/{mytoken}'
+            # myip = self.get_myip()
+            # mytoken = os.getenv("VOC_PROXY_TOKEN")
+            # self.prefix = f'/hostip/{myip}:5000/vocproxy/{mytoken}'
+            self.prefix = f"/proxy/5000/"
         else:
-            self.prefix = ''
+            self.prefix = ""
+
+    def clear_prefix(self, s, prefix):
+        if len(prefix) > 0 and s.startswith(prefix):
+            return self.clear_prefix(s[len(prefix)-1:], prefix)
+        else:
+            return s
 
     def __call__(self, environ, start_response):
-        print(environ['PATH_INFO'], self.prefix)
-        environ['SCRIPT_NAME'] = self.prefix
+        environ["PATH_INFO"] = self.clear_prefix(
+            environ["PATH_INFO"], self.prefix
+        )
+        environ["SCRIPT_NAME"] = self.prefix
         return self.app(environ, start_response)
 
     def get_myip(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(('8.8.8.8', 53))
+        s.connect(("8.8.8.8", 53))
         return s.getsockname()[0]
